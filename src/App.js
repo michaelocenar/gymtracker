9adfed37702cd75771a7c1c4af98ba340552d687
Mark Pineda <markpineda1@outlook.com>
2023-05-04 23:11:53 -0600

    modify the paragraph file name to poop.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/poop.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -25,3 +25,7 @@ function App() {
 export default App;
 
 //something funny
+// hello!
+// is this how we discuss changes to the app? 
+// no that's usually done through pull requests
+// ah that makes sense! 
