43516ab5486cdd29183805b3f568c98ea37b9d56
Mark Pineda <markpineda1@outlook.com>
2023-05-05 00:03:50 -0600

    add a counter that cycles from 0 to 100 by a 10 point increment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  // Below is basically what line 6 is without destructuring
+  // const counterState = useState(1);
+  // const counter = counterState[0];
+  // const setCounter = counterstate[1];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => (prevCounter >= 100 ? 0 : prevCounter + 10));
+    }, 1);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +26,7 @@ function App() {
         <p>
           Edit <code>src/poop.js</code> and save to reload.
         </p>
+        <p>Counter: {counter}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +37,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
