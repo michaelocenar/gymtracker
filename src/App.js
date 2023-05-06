e8d097cae5a5fb18241781bda309ebe9def4e397
Mark Pineda <markpineda1@outlook.com>
2023-05-05 20:20:14 -0600

    add a submit button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
 
   const [exercise, setExercise] = useState('');
   const [weight, setWeight] = useState(0);
+  const [repetition, setRepetition] = useState(0);
 
   const exercises = [
     "squat", 
@@ -18,16 +19,19 @@ const App = () => {
     e.preventDefault();
     console.log("Exercise", exercise);
     console.log("Weight", weight);
+    console.log("Repetition", repetition);
   }
   
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <select value={exercise} onChange={(e) => setExercise(e.target.value)}>
-          <option value=""> Select an exercise </option>
-          {exercises.map((ex) => (
+        <select 
+          value={exercise} 
+          onChange={(e) => setExercise(e.target.value)}>
+            <option value=""> Select an exercise </option>
+              {exercises.map((ex) => (
             <option key={ex} value={ex}>{ex}</option>
-          ))}
+            ))}
         </select>
         <input
           type="number"
@@ -35,6 +39,16 @@ const App = () => {
           value={weight} 
           onChange={(e) => setWeight(e.target.value)}
         />
+          <input
+          type="number"
+          placeholder="Enter repetitions"
+          value={repetition} 
+          onChange={(e) => setRepetition(e.target.value)}
+          min="1"
+          max="100"
+          step="1"
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
