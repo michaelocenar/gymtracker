e6463e0765ac93b34e91ec8edb91292cd8d642ef
Mark Pineda <markpineda1@outlook.com>
2023-05-05 19:54:21 -0600

    add a dropdown for exercises

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [exercise, setExercise] = useState('');
+  
+  const exercises = [
+    "squat", 
+    "bench press", 
+    "deadlift",
+    "lunges",
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Exercise", exercise);
+  }
   
   return (
     <div className="App">
+      <form onSubmit={handleSubmit}>
+        <select value={exercise} onChange={(e) => setExercise(e.target.value)}>
+          <option value=""> Select an exercise </option>
+          {exercises.map((ex) => (
+            <option key={ex} value={ex}>{ex}</option>
+          ))}
+        </select>
+      </form>
     </div>
-    
   );
 }
 
