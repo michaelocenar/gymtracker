595e14f3b7e8e132da9927e50d9301253f86640c
Mark Pineda <markpineda1@outlook.com>
2023-05-08 18:37:23 -0600

    modify dynoweight

--- src/App.js
+++ src/App.js
@@ -6,20 +6,31 @@ const App = () => {
   const [exercise, setExercise] = useState('');
   const [weight, setWeight] = useState(0);
   const [repetition, setRepetition] = useState(0);
+  const [exSets, setExSets] = useState([
+    {dynoweight:0,dynorep:0}
+  ]);
 
   const exercises = [
     "squat", 
     "bench press", 
     "deadlift",
-    "lunges",
   ];
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Exercise", exercise);
-    console.log("Weight", weight);
-    console.log("Repetition", repetition);
+    // console.log("Weight", weight);
+    // console.log("Repetition", repetition);
+    //console.log("DynoWeight", dynoWeight);
+    //console.log("DynoRep",dynoRep);
+  }
+
+  const handleAddSet = (index,event) => {
+    // add the current weight and repetition values as a new set to the sets state
+    let set = [...exSets];
+    set[index][event.target.name] = event.target.value;
+    // Reset weight and rep input to initial values
+    setExSets(set);
   }
   
   return (
@@ -33,13 +44,13 @@ const App = () => {
             <option key={ex} value={ex}>{ex}</option>
             ))}
         </select>
-        <input
+        {/* <input
           type="number"
           placeholder="Enter weight in kgs"
           value={weight} 
           onChange={(e) => setWeight(e.target.value)}
         />
-          <input
+        <input
           type="number"
           placeholder="Enter repetitions"
           value={repetition} 
@@ -47,7 +58,31 @@ const App = () => {
           min="1"
           max="100"
           step="1"
-        />
+        /> */}
+        {exSets.map((input,index) => {
+          return (
+            <div key={index}>
+              <input
+                type="number"
+                dynoWeight='weight'
+                placeholder='Weight'
+                value={input.dynoweight}
+                min="0"
+                onChange={event => handleAddSet(index, event)}
+              />
+              <input
+                type="number"
+                dynoRep='rep'
+                placeholder='Repetition'
+                value={input.dynorep}
+                min="1"
+                max="100"
+                step="1"
+                onChange={event => handleAddSet(index, event)}
+              />
+            </div>
+          )
+          })}
         <button type="submit">Submit</button>
       </form>
     </div>
